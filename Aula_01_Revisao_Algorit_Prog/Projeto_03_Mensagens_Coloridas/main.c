b3ed9d628e6b02679c3a7b6501358083d4d98cc7
Fernando Esquirio Torres <esquirio@gmail.com>
2020-08-26 13:33:51 -0300

    Atualização Aula 01

---
+++ Aula_01_Revisao_Algorit_Prog/Projeto_03_Mensagens_Coloridas/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>     // Necessário para usar printf
+#include <windows.h>   // Biblioteca de API do Windows
+
+/* Função:    int main() */
+int main()
+{
+  // Função SetConsoleTextAttribute para configurar a cor do console
+  // A color é definida por um número de 0 a 255
+  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+  // Mensagem escrita de verde
+  printf("I want to be nice today!\n");
+  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+  // Mensagem escrita de amarelo
+  printf("I want to be nice today!\n");
+  return 0; // Retorna um valor inteiro
+}// Fim da função int main()
+
+  /*
+  //No Linux
+  http://web.theurbanpenguin.com/adding-color-to-your-output-from-c/
+
+  printf("\033[0;31m"); //Set the text to the color red
+  printf("Hello\n"); //Display Hello in red
+  printf("\033[0m"); //Resets the text to default color
+  */
---
+++ Aula_01_Revisao_Algorit_Prog/Projeto_04_Caractere_Valor/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>                  // Necessário para usar printf
+
+/* Função:    int main()*/
+int main()
+{
+  char letra;                       // Cria variável char com o valor A
+  printf("Digite um caractere: ");  // Interação com o usuário
+  scanf("%c", &letra);              // Recebe um valor do teclado
+  printf("\n%c", letra);            // Imprime o caractere
+  printf("\n%d", letra);            // Imprime em decimal
+  printf("\n%x", letra);            // Imprime em hexadecimal
+  printf("\n%o", letra);            // Imprime em octal
+  return 0;                         // Retorna um valor inteiro
+}// Fim da função int main()
+
