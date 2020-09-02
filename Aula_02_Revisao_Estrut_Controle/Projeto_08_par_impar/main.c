64b9510c81abee1d72967dfe99d24ded2517e864
Fernando Esquirio Torres <esquirio@gmail.com>
2020-09-02 11:00:16 -0300

    Carrect

--- Aula_02_Revisao_Estrut_Controle/Projeto_08_par_impar/main.c
+++ Aula_02_Revisao_Estrut_Controle/Projeto_08_par_impar/main.c
@@ -1,23 +1,42 @@
-#include <stdio.h> // Funções de I/O, etc.
-
+#include <stdio.h>  // Funções de I/O, etc.
+#include <locale.h> // Mudar idioma
 /* Funcão: main() */
 int main(){
-  int num;
-  printf("Digite um numero: ");
-  scanf("%i", &num);
-  // Testa se número é par ou ímpar
-  if(!(num%2))
-    printf("Numero eh par!\n");
-  else
-    printf("Numero eh impar!\n");
+  char oper;
+  // Configura a língua portuguesa
+  setlocale(LC_ALL, "Portuguese");
+  printf("Digite um sinal de operação matemático: ");
+  scanf(" %c", &oper);
+  // Testa a operação
+  switch(oper){
+  case '+':
+    printf("Sinal de adição!\n");
+    break;
+  case '-':
+    printf("Sinal de subtração!\n");
+    break;
+  case '*':
+    printf("Sinal de multiplicação!\n");
+    break;
+  case '/':
+    printf("Sinal de divisão!\n");
+    break;
+  default:
+    printf("Outra operação matemática!\n");
+    break;
+  }// Fim do switch
   return 0;
 }// Fim da função main()
 
-/* Exercício 1A
-  if(num%2 == 0){
-    printf("Numero eh par!\n");
-  }// Fim do if()
-  else{
-    printf("Numero eh impar!\n");
-  }// Fim do else
+/* Exercício 2A
+  if(oper == '+')
+    printf("Sinal de adição!\n");
+  else if(oper == '-')
+    printf("Sinal de subtração!\n");
+  else if(oper == '*')
+    printf("Sinal de multiplicação!\n");
+  else if(oper == '/')
+    printf("Sinal de divisão!\n");
+  else
+    printf("Outra operação matemática!\n");
 */
